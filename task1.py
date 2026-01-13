7e9154caac8db9c1c0a1e6d5f8bfb62ca399aed2
Vishal Jangid <vishalsuthar9509@gmail.com>
2026-01-13 20:56:41 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,11 @@
+a = float(input("enter first number"))
+b = float(input("enter second number"))
+
+c = a+b
+print("add", c)
+c = a-b
+print("sub", c)
+c = a*b
+print("multi", c)
+c = a/b
+print("div", c)
---
+++ task2.py
@@ -0,0 +1,4 @@
+name = input("enter your first name:")
+last = input("enter your last name:")
+
+print("hello", name, last,"welcome to the python program.")
